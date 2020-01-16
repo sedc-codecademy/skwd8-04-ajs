94a12eddd0090c7ab863ea60d5d330446e1e6ef3
Alek Kocevski <34724137+Alekot@users.noreply.github.com>
2020-01-16 20:49:26 +0100

    student js new version

--- g1/Class2-Exercises/Code/student.js
+++ g1/Class2-Exercises/Code/student.js
@@ -23,13 +23,19 @@ function Student(firstName, lastName, birthYear, academy, grades){
     }
 }
 
+let listOfStudents = [];
+
 let petar = new Student('Petar', 'Petrovski', 1990, 'SEDC', [1, 2, 5]);
-console.log('Petar:', petar)
+listOfStudents.push(petar);
+
+// console.log('Petar:', petar)
 let petarsAge = petar.getAge();
-console.log(petar.getGradesAverage());
+// console.log(petar.getGradesAverage());
 
 let stojan = new Student('Stojan', 'Stojanovski', 1903, 'SEDC', [2,5,4,5,1,2]);
+listOfStudents.push(stojan);
 
+// console.log(listOfStudents);
 // with saving
 // console.log(petarsAge)
 
@@ -37,3 +43,59 @@ let stojan = new Student('Stojan', 'Stojanovski', 1903, 'SEDC', [2,5,4,5,1,2]);
 // console.log(petar.getAge())
 
 // console.log(petar.getInfo());
+
+// ELEMENTS
+let fName = document.getElementById('fName');
+let lName = document.getElementById('lName');
+let birthYear = document.getElementById('birthYear');
+let academy = document.getElementById('academy');
+let grades = document.getElementById('grades');
+let registerBtn = document.getElementById('register');
+
+let tBody = document.querySelector('tbody');
+
+// EVENT 
+registerBtn.addEventListener('click', function() {
+    tBody.innerHTML = '';
+    let firstName = fName.value;
+    let lastName = lName.value;
+    let birthYearValue = birthYear.value;
+    let academyValue = academy.value;
+    let gradesValue = grades.value.split(','); // split returns an array of strings;
+    let student = new Student(firstName,lastName, birthYearValue, academyValue, gradesValue)
+
+    listOfStudents.push(student);
+
+    for (const student of listOfStudents) {
+        // OPTION 1
+        // tBody.innerHTML +=   `<tr>
+        //                         <td>${student.firstName}</td>
+        //                         <td>${student.lastName}</td>
+        //                         <td>${student.birthYear}</td>
+        //                         <td>${student.academy}</td>
+        //                         <td>${student.grades}</td>
+        //                     </tr>`
+
+        // OPTION 2
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        let td4 = document.createElement('td');
+        let td5 = document.createElement('td');
+
+        td1.innerText = student.firstName;
+        td2.innerText = student.lastName;
+        td3.innerText = student.birthYear;
+        td4.innerText = student.academy;
+        td5.innerText = student.grades;
+        
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        
+        tBody.appendChild(tr);
+    }
+})
