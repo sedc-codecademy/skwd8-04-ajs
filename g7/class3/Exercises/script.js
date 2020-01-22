9227885d4e0f52fbb46278953da78240fb6ff10b
dragan <dragang@iborn.net>
2020-01-22 20:58:45 +0100

    class 3 tuto finito 2

--- g7/class3/Exercises/script.js
+++ g7/class3/Exercises/script.js
@@ -25,42 +25,6 @@
 // })
 
 // Okay way
-$(document).ready(function(){
-    let callBtn = document.getElementById("callBtn");
-    let result = document.getElementById("result");
-    callBtn.addEventListener("click", function(){
-        $.ajax({
-            url:"https://raw.githubusercontent.com/sedc-codecademy/skwd8-04-ajs/master/g7/class3/students.json",
-            success: function(response){
-                console.log("Success!");
-                let responseObject = JSON.parse(response);
-                let resultTitle = result.getElementsByTagName("h1")[0];
-                let resultList = result.getElementsByTagName("ul")[0];
-                resultTitle.innerText = `${responseObject.academy} Academy:`
-                resultList.innerHTML = "";
-                for (let student of responseObject.students) {
-                    resultList.innerHTML += `<li>${student}</li>`
-                }
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-    })
-})
-
-// // Good way
-// function printAcademy(resultElement, academy){
-//     let resultTitle = resultElement.getElementsByTagName("h1")[0];
-//     resultTitle.innerText = `${academy} Academy:`
-// }
-// function printStudents(resultElement, students){
-//     let resultList = resultElement.getElementsByTagName("ul")[0];
-//     resultList.innerHTML = "";
-//     for (let student of students) {
-//         resultList.innerHTML += `<li>${student}</li>`
-//     }
-// }
 // $(document).ready(function(){
 //     let callBtn = document.getElementById("callBtn");
 //     let result = document.getElementById("result");
@@ -70,8 +34,13 @@ $(document).ready(function(){
 //             success: function(response){
 //                 console.log("Success!");
 //                 let responseObject = JSON.parse(response);
-//                 printAcademy(result, responseObject.Academy);
-//                 printStudents(result, responseObject.students);
+//                 let resultTitle = result.getElementsByTagName("h1")[0];
+//                 let resultList = result.getElementsByTagName("ul")[0];
+//                 resultTitle.innerText = `${responseObject.academy} Academy:`
+//                 resultList.innerHTML = "";
+//                 for (let student of responseObject.students) {
+//                     resultList.innerHTML += `<li>${student}</li>`
+//                 }
 //             },
 //             error: function(error){
 //                 console.log(error);
@@ -79,3 +48,34 @@ $(document).ready(function(){
 //         })
 //     })
 // })
+
+// // Good way
+function printAcademy(resultElement, academy){
+    let resultTitle = resultElement.getElementsByTagName("h1")[0];
+    resultTitle.innerText = `${academy} Academy:`
+}
+function printStudents(resultElement, students){
+    let resultList = resultElement.getElementsByTagName("ul")[0];
+    resultList.innerHTML = "";
+    for (let student of students) {
+        resultList.innerHTML += `<li>${student}</li>`
+    }
+}
+$(document).ready(function(){
+    let callBtn = document.getElementById("callBtn");
+    let result = document.getElementById("result");
+    callBtn.addEventListener("click", function(){
+        $.ajax({
+            url:"https://raw.githubusercontent.com/sedc-codecademy/skwd8-04-ajs/master/g7/class3/students.json",
+            success: function(response){
+                console.log("Success!");
+                let responseObject = JSON.parse(response);
+                printAcademy(result, responseObject.Academy);
+                printStudents(result, responseObject.students);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+    })
+})
