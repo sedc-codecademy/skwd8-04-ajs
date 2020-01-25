95853712467b14731f2d31205aaec2d0d5c6f7a9
Stojancho Jefremov <stojancho.jefremov@seavus.com>
2020-01-25 13:04:09 +0100

    [G6] - Class4 - scopes script modified

--- g6/Class04/js/scope.js
+++ g6/Class04/js/scope.js
@@ -7,8 +7,8 @@
 // console.log(result);//throws error Uncaught ReferenceError: result is not defined
 
 
-//with let we have block scope (variable lives inside the brackets, starting from where it is declared)
-//with var we have functional scope (variable lives inside the function it is declared, starting from function start, but value is set on the initialization)
+// with let we have block scope (variable lives inside the brackets, starting from where it is declared)
+// with var we have functional scope (variable lives inside the function it is declared, starting from function start, but value is set on the initialization)
 // function testScopes() {
 //     console.log("variableWithFunctionScope before declaration: " + variableWithFunctionScope);
 //     console.log("variableWithBlockScope before declaration: " + variableWithBlockScope);
@@ -23,8 +23,8 @@
 
 
 //testing global scope
-//console.log(`globalBlockScopeVariable: ${globalBlockScopeVariable}`);//throws error
 console.log(`globalFunctionScopeVariable: ${globalFunctionScopeVariable}`);
+console.log(`globalBlockScopeVariable: ${globalBlockScopeVariable}`);//throws error
 
 let globalBlockScopeVariable = "globalBlockScopeVariable";
 var globalFunctionScopeVariable = "globalFunctionScopeVariable";
