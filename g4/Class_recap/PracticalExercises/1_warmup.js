fd80cfc646a1b54dc873374ee80116edeb31c183
viktorjakovlev <viktorjakovlev@hotmail.com>
2020-02-01 11:57:00 +0100

    practical exercise added

---
+++ g4/Class_recap/PracticalExercises/1_warmup.js
@@ -0,0 +1,54 @@
+// Task 1
+//Declare a variable a.
+//Assign the variable with the value of 7.
+//Declare and asign a variable b with the value of 8.
+//Declare a variable c and asign it the value of a + b.
+//Print the value in the console.
+
+// Task 2
+//Declare and asign an integer variable.
+//Declare and asign a boolean variable.
+//Declare and asign a string variable.
+//Use the typeof operator to check the types of all assigned variables.
+
+// Task 3
+//Declare 2 variables x and y and set them to 10 and 3.
+//Declare and asign a variable that would be the result of division of 10 and 3.
+//Declare and asign a variable that would be the whole part of the division of 10 and 3.
+//Declare and asign a variable that would be the remainder of the division of 10 and 3.
+
+// Task 4
+//Declare a variable name.
+//Assign the variable to your name.
+//Declare and assign a variable lastName with the value of your last name.
+//Print Hello, and your name and last name in the console from the previous variables.
+
+// Task 5
+//Declare two number variables and assign them any number
+//Write an if statement that prints the larger one in console
+
+// Task 6
+//Declare a variable and assign any number to it
+//Write an if statement that finds out if a number is even OR larger than 100
+//If it is print in the console: The number is cool!
+
+// Task 7
+//Write a while loop that counts from 0 to 10.
+//The numbers should be printed in the console.
+
+// Task 8
+//Write a for loop that counts from 1 to 20 but skips every second number.
+//The numbers should be printed in the console.
+
+// Task 9
+//Write a function that prints your name in the console
+
+// Task 10
+//Write a function that accepts your name as an argument and returns "Hello NAME"
+
+// Task 11
+//Write a function that accepts 2 numbers as arguments and returns the sum of the two
+
+//Task 12
+//Create a car Object with object literal nontation with minimum od 4 properties and 1 method, 
+//and print every ptoperty in the console using the previously created method
---
+++ g4/Class_recap/PracticalExercises/3_advanced.js
@@ -0,0 +1,56 @@
+//Task 1
+//Write a JavaScript program to check whether a matrix is a diagonal matrix or not. 
+//In linear algebra, a diagonal matrix is a matrix in which the entries outside 
+//the main diagonal are all zero (the diagonal from the upper left to the lower right).
+//Hint: Matrix == 2 dimensional array(array of arrays)
+
+//Task 2
+//Write a JavaScript program to remove duplicate items from an array
+
+//Task 3
+//Write a JavaScript program which accept a number as input and insert dashes (-) between 
+//each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.
+
+//Task 4
+//Write a JavaScript program which prints the elements of the following array. Note: Use nested for loops.
+//Sample array: var a = [[1, 2], [8, 11], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+//Task 5
+//Find all the palindroms in the array. A palindrom is a word that is equal to the reversed word. Ex : "Ana" , "Bob" "boob"
+
+//Task 6
+//Create a new array that the elements are the lenghts of the elements in the previous array.
+
+//BONUS
+
+//Task FizzBuzz
+//Part 1
+//Create a function that will take a number as parameter and print in console all the numbers to that number.
+//number divisible by 3 print Fizz
+//number divisible by 5 print Buzz
+//number divisible by 3 and divisible by 5 print FizzBuzz
+//Part 2
+//Add input in the HTML and a button. On click of the button print the values up to the entered number.
+
+
+//TASK Coin Denomination
+//Given these coin denominations: 1¢, 5¢, 10¢, 20¢, 50¢, find the smallest number of coins needed for a given amount
+//Example 1: For 375 cents, 9 coins are needed
+//Example 2: For 543 cents, 15 coins are needed
+//bonus1 - Make the finite amount for each coin
+//bonus2 - Make a report how many coins of each kind were used to return a change
+//bonus3 - Make an UI :)
+
+
+//TASK Number Checker
+//Part 1
+//Write a function that will give stats about an integer number.
+//The function should return an object with the stats as properties. The stats required are:
+
+//1. Whats the value of the number (value property)
+//2. How many digits the number has (numDigits property)
+//3. Is the number odd or even (isEven property)
+//4. Is the number positive (isPositive property)
+//Part 2
+//Write user interface for the number checker. It should include an entry field for the number as well as a 
+//button to trigger the calculation. After the calculation, and appropriate response should be shown on the page.
