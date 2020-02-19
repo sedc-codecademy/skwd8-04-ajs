fed503a55664f45b3bab5e76c04be65d2a4dd15b
Martin Panovski <martinp@iborn.net>
2020-02-19 21:03:46 +0100

    StarWarsApp refactored final version added

--- g2/Class13/StarWarsApp - Refactored/scripts/app.js
+++ g2/Class13/StarWarsApp - Refactored/scripts/app.js
@@ -1,193 +1,148 @@
-// alert("Connection test!")
-const btnPeople = document.getElementById("btnPeople");
-const btnShips = document.getElementById("btnShips");
-const result = document.getElementById("result");
-const loader = document.getElementById("loader");
-const btnNext = document.getElementById("btnNext");
-const btnPrev = document.getElementById("btnPrev");
-
-//Global variables for dynamic manipulation of fetching the data
-const baseUrl = "https://swapi.co/api/";
-let pageMode = "";
-let currentPage = 1;
-
-const getPeople = page => {
-    const peopleUrl = `${baseUrl}people/?page=${page}`;
-    toggleLoader(true);
-    // $.ajax({
-    //     url: peopleUrl,
-    //     success: (people) => {
-    //         console.log("The request succeeded!", people);
-    //         toggleLoader(false);
-    //         toggleNavButtons(people);
-    //         displayPeople(people.results);
-    //     },
-    //     error: (err) => {
-    //         console.log(err);
-    //     }
-    // });
-    fetch(peopleUrl)
-        .then(res => res.json())
-        .then(people => {
-            console.log("The request succeeded!", people);
-            toggleLoader(false);
-            toggleNavButtons(people);
-            displayPeople(people.results);
-        })
-        .catch(err => console.log(err));
-}
-
-const   = people => {
-    if (people != null) {
-        result.innerHTML = '';
-        result.innerHTML += `
-        <div class="row">
-            <div class="col-md-3">Name</div>
-            <div class="col-md-2">Height</div>
-            <div class="col-md-2">Mass</div>
-            <div class="col-md-2">Gender</div>
-            <div class="col-md-2">Birth year</div>
-            <div class="col-md-1">Films</div>
-        </div>`;
-        for (const person of people) {
-            result.innerHTML += `
-        <div class="row">
-            <div class="col-md-3">${person.name}</div>
-            <div class="col-md-2">${person.height}</div>
-            <div class="col-md-2">${person.mass}</div>
-            <div class="col-md-2">${person.gender}</div>
-            <div class="col-md-2">${person.birth_year}</div>
-            <div class="col-md-1">${person.films.length}</div>
-        </div>`;
+let navigationService = {
+    btnPeople: document.getElementById("btnPeople"),
+    btnShips: document.getElementById("btnShips"),
+    btnNext: document.getElementById("btnNext"),
+    btnPrev: document.getElementById("btnPrev"),
+    pageMode: "",
+    currentPage: 1,
+    registerListeners: function () {
+        this.btnPeople.addEventListener("click", () => {
+            this.currentPage = 1;
+           starWarsService.getPeople(this.currentPage);
+            this.pageMode = "people";
+        });
+        this.btnShips.addEventListener("click", () => {
+            this.currentPage = 1;
+            starWarsService.getShips(this.currentPage);
+            this.pageMode = "ships";
+        });
+        this.btnNext.addEventListener("click", () => {
+            this.getNextPage(this.pageMode);
+        });
+        this.btnPrev.addEventListener("click", () => {
+            this.getPreviousPage(this.pageMode);
+        });
+    },
+    getNextPage: function (pageType) {
+        ++this.currentPage;
+        pageType === "people" ? starWarsService.getPeople(this.currentPage) : null;
+        pageType === "ships" ? starWarsService.getShips(this.currentPage) : null;
+    },
+    getPreviousPage: function (pageType) {
+        --this.currentPage;
+        pageType === "people" ? starWarsService.getPeople(this.currentPage) : null;
+        pageType === "ships" ? starWarsService.getShips(this.currentPage) : null;
+    },
+    toggleNavButtons: function (response) {
+        if (!response.next) {
+            this.btnNext.style.display = "none";
+        } else {
+            this.btnNext.style.display = "block";
         }
-    } else {
-        result.innerHTML += '<h2>There is something wrong with the data!</h2>'
-    }
-}
 
-const getShips = page => {
-    const shipsUrl = `${baseUrl}starships/?page=${page}`;
-    toggleLoader(true);
-    fetch(shipsUrl)
-        .then(res => res.json())
-        .then(ships => {
-            console.log("The request succeeded!", ships);
-            toggleLoader(false);
-            toggleNavButtons(ships);
-            displayShips(ships.results);
-        })
-        .catch(err => console.log(err));
-}
-
-const displayShips = ships => {
-    if (ships != null) {
-        result.innerHTML = '';
-        result.innerHTML += `
-        <div class="row">
-            <div class="col-md-2">Name</div>
-            <div class="col-md-2">Model</div>
-            <div class="col-md-2">Manufacturer</div>
-            <div class="col-md-2">Cost</div>
-            <div class="col-md-2">People Capacity</div>
-            <div class="col-md-2">Class</div>
-        </div>
-        <hr color="green">`;
-        for (const ship of ships) {
-            result.innerHTML += `
-        <div class="row">
-            <div class="col-md-2">${ship.name}</div>
-            <div class="col-md-2">${ship.model}</div>
-            <div class="col-md-2">${ship.manufacturer}</div>
-            <div class="col-md-2">${ship.cost_in_credits}</div>
-            <div class="col-md-2">${parseInt(ship.passengers) + parseInt(ship.crew)}</div>
-            <div class="col-md-2">${ship.starship_class}</div>
-        </div>
-        <hr color="yellow">`;
+        if (!response.previous) {
+            this.btnPrev.style.display = "none";
+        } else {
+            this.btnPrev.style.display = "block";
         }
-    } else {
-        result.innerHTML += '<h2>There is something wrong with the data!</h2>'
     }
 }
 
-const toggleLoader = toggle => {
-    if (toggle)
-        loader.style.display = 'block';
-    else
-        loader.style.display = 'none';
-}
-
-const toggleNavButtons = response => {
-    if (response.next === null) {
-        btnNext.style.display = "none";
-    } else {
-        btnNext.style.display = "block";
+let starWarsService = {
+    baseUrl: "https://swapi.co/api/",
+    getPeople: function (page) {
+        const peopleUrl = `${this.baseUrl}people/?page=${page}`;
+        uiService.toggleLoader(true);
+        fetch(peopleUrl)
+            .then(res => res.json())
+            .then(people => {
+                console.log("The request succeeded!", people);
+                uiService.toggleLoader(false);
+                navigationService.toggleNavButtons(people);
+               uiService.displayPeople(people.results);
+            })
+            .catch(err => console.log(err));
+    },
+    getShips: function (page) {
+        const shipsUrl = `${this.baseUrl}starships/?page=${page}`;
+        uiService.toggleLoader(true);
+        fetch(shipsUrl)
+            .then(res => res.json())
+            .then(ships => {
+                console.log("The request succeeded!", ships);
+                uiService.toggleLoader(false);
+                navigationService.toggleNavButtons(ships);
+                uiService.displayShips(ships.results);
+            })
+            .catch(err => console.log(err));
     }
+}
 
-    if (response.previous === null) {
-        btnPrev.style.display = "none";
-    } else {
-        btnPrev.style.display = "block";
+let uiService = {
+    result: document.getElementById("result"),
+    loader: document.getElementById("loader"),
+    toggleLoader: function (toggle) {
+        if (toggle)
+            this.loader.style.display = 'block';
+        else
+            this.loader.style.display = 'none';
+    },
+    displayPeople: function (people) {
+        if (people) {
+            this.result.innerHTML = '';
+            this.result.innerHTML += `
+            <div class="row">
+                <div class="col-md-3">Name</div>
+                <div class="col-md-2">Height</div>
+                <div class="col-md-2">Mass</div>
+                <div class="col-md-2">Gender</div>
+                <div class="col-md-2">Birth year</div>
+                <div class="col-md-1">Films</div>
+            </div>`;
+            for (const person of people) {
+                this.result.innerHTML += `
+            <div class="row">
+                <div class="col-md-3">${person.name}</div>
+                <div class="col-md-2">${person.height}</div>
+                <div class="col-md-2">${person.mass}</div>
+                <div class="col-md-2">${person.gender}</div>
+                <div class="col-md-2">${person.birth_year}</div>
+                <div class="col-md-1">${person.films.length}</div>
+            </div>`;
+            }
+        } else {
+            this.result.innerHTML += '<h2>There is something wrong with the data!</h2>'
+        }
+    },
+    displayShips: function(ships) {
+        if (ships) {
+            this.result.innerHTML = '';
+            this.result.innerHTML += `
+            <div class="row">
+                <div class="col-md-2">Name</div>
+                <div class="col-md-2">Model</div>
+                <div class="col-md-2">Manufacturer</div>
+                <div class="col-md-2">Cost</div>
+                <div class="col-md-2">People Capacity</div>
+                <div class="col-md-2">Class</div>
+            </div>
+            <hr color="green">`;
+            for (const ship of ships) {
+                this.result.innerHTML += `
+            <div class="row">
+                <div class="col-md-2">${ship.name}</div>
+                <div class="col-md-2">${ship.model}</div>
+                <div class="col-md-2">${ship.manufacturer}</div>
+                <div class="col-md-2">${ship.cost_in_credits}</div>
+                <div class="col-md-2">${parseInt(ship.passengers) + parseInt(ship.crew)}</div>
+                <div class="col-md-2">${ship.starship_class}</div>
+            </div>
+            <hr color="yellow">`;
+            }
+        } else {
+            this.result.innerHTML += '<h2>There is something wrong with the data!</h2>'
+        }
     }
 }
 
-const getNextPage = pageType => {
-    ++currentPage;
-    pageType === "people" ? getPeople(currentPage) : null;
-    pageType === "ships" ? getShips(currentPage) : null;
-}
-
-const getPreviousPage = pageType => {
-    --currentPage;
-    pageType === "people" ? getPeople(currentPage) : null;
-    pageType === "ships" ? getShips(currentPage) : null;
-}
-
-btnPeople.addEventListener("click", () => {
-    currentPage = 1;
-    getPeople(currentPage);
-    pageMode = "people";
-});
-
-btnShips.addEventListener("click", () => {
-    currentPage = 1;
-    getShips(currentPage);
-    pageMode = "ships";
-});
-
-btnNext.addEventListener("click", () => {
-    getNextPage(pageMode);
-});
-
-btnPrev.addEventListener("click", () => {
-    getPreviousPage(pageMode);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function throwError(num) {
-//     if (num > 5) {
-//         console.log(num);
-//     } else {
-//         throw new Error("The number should be greater than 5!");
-//     }
-// }
-
-
-// try {
-//     throwError(3);
-// } catch (error) {
-//     console.log(error)
-// }
-
+navigationService.registerListeners();
